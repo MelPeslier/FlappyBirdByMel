0cfbebf1c7d82479828714d1f6e0d1b0c1a5d11c
MelPeslier <mel.peslier@gmail.com>
2023-04-24 18:24:33 -0300

    Rajout : appel du signal 'bounce'

--- scripts/sol.gd
+++ scripts/sol.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 func _on_area_entered(area):
 	
 	area._die()
@@ -8,3 +7,5 @@ func _on_area_entered(area):
 	area._jump_force *= 0.9
 	area._nb_bumps -= 1
 	area._velocity.y = - area._jump_force
+	
+	Events.emit_signal("bounce", area.max_nb_bumps, area._nb_bumps)
